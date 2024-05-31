0bd42fc2cc6a937c8a248756cdaf2f77e899ec92
Tabitha <beckfoote@gmail.com>
2024-05-31 07:53:02 -0400

    workable version of the imageAndTextBlock compnenet with circular images

--- src/components/ImageAndTextBlock.tsx
+++ src/components/ImageAndTextBlock.tsx
@@ -5,26 +5,50 @@ import Paragraph from "./Paragraph";
 import "../base.css";
 
 type Props = {
-    heading: string;
-    text: string;
-    direction: 'left' | 'right';
-    background: 'light' | 'medium' | 'dark';
-    className: string;
-    // we will also need the image eventually
-}
+  heading: string;
+  text: string;
+  direction: "left" | "right";
+  background: "light" | "medium" | "dark";
+  className: string;
+  // we will also need the image eventually
+};
 
-const ImageAndTextBlock: React.FC<Props> = ({ heading, text, direction, background, className }) => {
-    return <section className={cx("relative md:flex", `bg-${background}-pink`, direction === "right" ? "flex-row" : "flex-row-reverse", className)}>
-        <div className="md:w-2/3 md:py-8 xl:py-12">
-            <div>
-                <Header size="medium" fancy>{heading}</Header>
-                <Paragraph>{text}</Paragraph>
-            </div>
+const ImageAndTextBlock: React.FC<Props> = ({
+  heading,
+  text,
+  direction,
+  background,
+  className,
+}) => {
+  return (
+    <section
+      className={cx(
+        "relative md:flex",
+        `bg-${background}-pink`,
+        direction === "right" ? "flex-row" : "flex-row-reverse",
+        className
+      )}
+    >
+      <div className="md:w-2/3 md:py-8 xl:py-12">
+        <div>
+          <Header size="medium" fancy>
+            {heading}
+          </Header>
+          <Paragraph>{text}</Paragraph>
         </div>
+      </div>
 
-        {/* I know it's an absolute height. But it's only for small screens so is it okay? */}
-        <div className="h-[300px] bg-grass bg-cover md:w-1/3 md:h-auto"></div>
-    </section>;
-}
+      {/* I know it's an absolute height. But it's only for small screens so is it okay? */}
+      {/* <div className="h-[300px] bg-grass bg-cover md:w-1/3 md:h-auto"></div> */}
+      <div className="md:w-1/3 md:flex md:items-center md:justify-center">
+        <img
+          className="h-[300px] w-full object-cover md:w-5/6 md:rounded-full md:h-auto md:aspect-square md:myx-8 lg:w-[304px] xl:myx-12"
+          src="/src/images/grass.jpg"
+          alt="grass"
+        />
+      </div>
+    </section>
+  );
+};
 
 export default ImageAndTextBlock;
--- src/components/ThreeImageBlock.tsx
+++ src/components/ThreeImageBlock.tsx
@@ -6,28 +6,27 @@ const ThreeImageBlock: React.FC = () => {
   return (
     <>
       <ImageAndTextBlock
-        className="-mbx-10 z-[2] md:clip-test"
+        className=""
         heading="Something"
         direction="right"
         background="dark"
         text="Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
       />
 
-      {/* <div className="hidden z-[1] relative md:block h-28 bg-gradient-to-br from-dark-pink from-50% to-transparent to-50%"></div> */}
-      {/* <div className="hidden relative z-[3] -mt-28 md:block h-28 bg-gradient-to-br from-transparent from-50% to-medium-pink to-50%"></div> 9px */}
+      <div className="hidden  md:block h-28 bg-gradient-to-br from-dark-pink from-50% to-medium-pink to-50%"></div>
 
       <ImageAndTextBlock
-        className="z-[11x] md:-mxt-20 md:clip-test2"
+        className=""
         heading="Something Else"
         direction="left"
         background="medium"
         text="Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
       />
 
-      {/* <div className="hidden relative z-10 md:block h-28 bg-gradient-to-bl from-medium-pink from-50% to-light-pink to-50%"></div> */}
+      <div className="hidden md:block h-28 bg-gradient-to-bl from-medium-pink from-50% to-light-pink to-50%"></div>
 
       <ImageAndTextBlock
-        className="md:clip-test3"
+        className=""
         heading="A Third Something"
         direction="right"
         background="light"
