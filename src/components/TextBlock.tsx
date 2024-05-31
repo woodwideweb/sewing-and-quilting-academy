747485cb8cd3ec37f79a3a9581308dc355ad63d7
Tabitha <beckfoote@gmail.com>
2024-05-31 07:56:44 -0400

    fixed isue with TextBlock

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -5,21 +5,29 @@ import "../base.css";
 
 // remove props if this is a one-off component that I don't reuse, though I suspect it won't be
 type Props = {
-    heading: string;
-    text: string;
-}
+  heading: string;
+  text: string;
+};
 
 const TextBlock: React.FC<Props> = ({ heading, text }) => {
-    return <section className="bg-light-pink flex xl:py-8">
-        <div className="flex items-center justify-center lg:w-1/4">
-            <img className="rounded-full aspect-square object-cover w-1/3 mt-8 sm:w-2/3 lg:w-[265px]" src="src/images/Flower.jpg" alt="flowers" />
-        </div>
+  return (
+    <section className="bg-light-pink flex flex-col md:flex-row xl:py-8">
+      <div className="flex items-center justify-center lg:w-1/4">
+        <img
+          className="rounded-full aspect-square object-cover w-1/3 mt-8 sm:w-[184px] md:w-2/3 lg:w-[265px]"
+          src="src/images/Flower.jpg"
+          alt="flowers"
+        />
+      </div>
 
-        <div className="lg:w-3/4">
-            <Header size="medium" fancy className="text-center sm:text-left">{heading}</Header>
-            <Paragraph>{text}</Paragraph>
-        </div>
+      <div className="lg:w-3/4">
+        <Header size="medium" fancy className="text-center sm:text-left">
+          {heading}
+        </Header>
+        <Paragraph>{text}</Paragraph>
+      </div>
     </section>
-}
+  );
+};
 
-export default TextBlock
+export default TextBlock;
