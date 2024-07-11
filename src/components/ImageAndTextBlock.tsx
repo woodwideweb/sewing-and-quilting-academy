bd3bbe746a6e617cd8c8631bd79a2a65ecc60aec
Tabitha <beckfoote@gmail.com>
2024-07-11 09:40:23 -0400

    hopefully fixed some images

--- src/components/ImageAndTextBlock.tsx
+++ src/components/ImageAndTextBlock.tsx
@@ -3,6 +3,7 @@ import cx from "../classnames";
 import Header from "./Header";
 import Paragraph from "./Paragraph";
 import "../base.css";
+import grass from "../images/grass.jpg";
 import Button from "./Button";
 
 type Props = {
@@ -50,7 +51,7 @@ const ImageAndTextBlock: React.FC<Props> = ({
         {/* I know it's an absolute height. But it's only for small screens so is it okay? */}
         <img
           className="h-[300px] w-full object-cover md:w-5/6 md:rounded-full md:h-auto md:aspect-square md:myx-8 lg:w-[304px] xl:myx-12"
-          src="../images/grass.jpg"
+          src={grass.src}
           alt="grass"
         />
       </div>
