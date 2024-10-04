735bfa64faf22c59d0f6ee27a1229bccd395287f
Tabitha <beckfoote@gmail.com>
2024-10-04 10:36:48 -0400

    made aesthetic picture change

--- src/components/HeroBlock.tsx
+++ src/components/HeroBlock.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./Header";
-import beach from "../images/beach.jpg";
 import sign from "../images/sign.jpg";
+import logo from "../images/logo.jpg";
+import machines from "../images/machines.jpg";
+import quilt1 from "../images/quilt-1.jpg";
 import "../base.css";
 import Paragraph from "./Paragraph";
 
@@ -22,7 +24,8 @@ const HeroBlock: React.FC = () => {
           {/* this thing stays on top of the header! Redardless of it's display!*/}
           <address className="mt-2 not-italic opacity-80 md:text-[17px] ">
             275 Main Street <br />
-            Wadsworth, Ohio
+            Wadsworth, Ohio <br />
+            330 780 1164
           </address>
         </Paragraph>
       </div>
@@ -30,7 +33,13 @@ const HeroBlock: React.FC = () => {
       <div
         className="bg-cover h-[50vh] md:w-2/3 md:h-[60vh] lg:h-[75vh]"
         // so it works at build time
-        style={{ backgroundImage: `url(${sign.src})` }}
+        // style={{ backgroundImage: `url(${sign.src})` }}
+        // style={{ backgroundImage: `url(${quilt1.src})` }}
+        // I know this isn't what they wanted, but I think that the sign looks bad
+        // I'll talk to them about it - if they could get a really nice picture of a project or machine
+        // that would be good too
+        style={{ backgroundImage: `url(${machines.src})` }}
+        // style={{ backgroundImage: `url(${logo.src})` }}
       >
         {/* <img src="src/images/beach.jpg" className="object-cover h-[50vh] lg:h-full lg:w-full" alt="a beach" /> */}
       </div>
--- src/components/ThreeImageBlock.tsx
+++ src/components/ThreeImageBlock.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ImageAndTextBlock from "./ImageAndTextBlock";
 import machines from "../images/machines.jpg";
 import notionBag from "../images/notions.jpg";
+import sign from "../images/sign.jpg";
 import "../base.css";
 
 const ThreeImageBlock: React.FC = () => {
@@ -12,7 +13,9 @@ const ThreeImageBlock: React.FC = () => {
         heading="Something"
         direction="right"
         // background="medium"
-        imgSrc={machines.src}
+        // imgSrc={machines.src}
+        // need a new picture - this is part of the experiment
+        imgSrc={sign.src}
         background="dark"
         text="Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet,fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
       />
